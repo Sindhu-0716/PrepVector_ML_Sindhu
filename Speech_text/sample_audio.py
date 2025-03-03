c07b60ad95200744c597ac6ffd099a16449e8872
sindhu <sindhug0716@gmail.com>
2025-03-02 20:33:05 -0800

    commit everything

--- Speech_text/sample_audio.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS # type: ignore
-
-text = "Hello! This is a sample audio file for testing transcription using Whisper."
-audio = gTTS(text)
-audio.save("sample_audio.mp3")
-
-print("Sample audio saved as 'sample_audio.mp3'")
